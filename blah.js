e784c542aca808837f3fde9bdc84a32e8f6b4486
njung92 <njjung@cmu.edu>
2014-10-06 02:05:01 -0400

    Update blah.js

--- blah.js
+++ blah.js
@@ -8,7 +8,7 @@ jQuery.ajax = (function(_ajax){
                       YQL      = 'http' + (/^https/.test(protocol)?'s':'') + '://query.yahooapis.com/v1/public/yql?callback=?',
                       query    = 'select * from html where url="{URL}" and xpath="*"';
                
-                  funct ion isExternal(url) {
+                  function isExternal(url) {
                     return !exRegex.test(url) && /:\/\//.test(url);
                   }
                   return function(o) {
@@ -58,8 +58,7 @@ $.ajax({
         var str = res.responseText;
         var m;
         var songList = new linkedList();
-        var songQueue = [];
-		var songInfo = [];
+        var songInfo = [];
         var i=0;
         var j=0;
  
@@ -72,70 +71,58 @@ $.ajax({
             songInfo[0] = m[1];
           } else {
             // Get the song title
-            songInfo[1] = m[2];
-            songQueue[j] = songInfo;
+            songInfo[1]=m[2];
             songList.add(new node(songInfo));
-            j++;
-			songInfo = [];
+            songInfo = [];
           }
           i++;
         }
-        updateNowPlaying(songList);
+        $("audio").on('ended', function() {
+          playNext(songList);
+        });
+        updateNowPlaying(songList, songList.curr.next);
         listSongs(songList);
      }
 });
 
-function updateNowPlaying(songList){
-  var title = songList.head.next.data[1];
-  var id = songList.head.next.data[0];
-  var audio = $('<audio controls type="audio/mpeg">');
-  var source = $('<source>');
-  displayHeadTitle(title);
-  source.src = getHeadSrc(songList);
-  audio.appendChild(source);
-  document.getElementById("player").appendChild(audio);
+function playNext(songList) {
+  updateNowPlaying(songList, songList.curr.next);
+}
+
+function updateNowPlaying(songList, song) {
+  songList.curr = song;
+  console.log("curr: ", songList.curr);
+  var title = song.data[1];
+  var id = song.data[0];
+  $("#nowPlaying").replaceWith("<p id='nowPlaying'>Now Playing: " + title + "</p>");
+  $("audio").attr("src", getSrc(song));
 }
 
 function listSongs(songList) {
   var n = songList.head.next;
   while (n.next != null) {
-    var data       = n.data;
-    var songId     = data[0];
-    var songTitle  = data[1];
-    var li       = document.createElement("li");
-    li.id        = songId;
-    var titleText  = document.createTextNode(songTitle);
-    li.appendChild(titleText);
-    document.getElementById("songList").appendChild(li);
-	}
+    var data = n.data;
+    var songId = data[0];
+    var songTitle = data[1];
+    $("#songList").append("<li id=" + songId + ">" + songTitle + "</li>");
+    $("#" + songId).click(function() {
+      updateNowPlaying(songList, songList.find(this.id));
+    });
+    n = n.next;
+  }
 }
 
-function displayHeadTitle(title) {
-  var titleNode = document.createTextNode("Now Playing: " + title);
-  document.getElementById("nowPlaying").appendChild(titleNode);
-}
-
-
-function getHeadSrc(songList) {
+function getSrc(song) {
   var link = 'http://www.brawlcustommusic.com/music/mp3/';
-  if (songList.head.next != null) {
-    var id = songList.head.next.data[0];
+  if (song.next != null) {
+    var id = song.data[0];
     var path = link + id + '.mp3';
+    console.log("src: " + path);
     return path;
   }
 }
 
-function removeSong(songList, songId) {
-  console.log("removing: " + songId);
+function removeSong(songQueue, id) {
   // Check if the song is currently playing
-  if (songList.head.next.data[0] == songId) {
-  }
   // It is not currently playing so just remove it from the list
-  else {
-	var n = songList.find(songId);
-	songList.remove(n);
-	var p = document.getElementById(songId);
-	p.parentNode.removeChild(p);
-  }
-		
-
+}
