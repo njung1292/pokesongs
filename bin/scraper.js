5bee2f751211137b3534a87ecbb0aa32ae6944fd
Noelle Jung <njung1292@gmail.com>
2014-10-12 16:53:56 -0400

    idk

--- bin/scraper.js
+++ bin/scraper.js
@@ -3,14 +3,36 @@
 var YQL = require('yql');
 var query = "select * from html where (url=@url) and (xpath=@xpath)";
 
+// module.exports = {
+//     makeQuery : function(url, xpath, callback) {
+//         console.log("making query...");
+//         var i = 0;
+
+//         while (true) {
+//             console.log("attempt: ", i);
+//             new YQL.exec(query, function(response) {
+//                 if (response.error) {
+//                     console.log("error with makeQuery: ", response.error.description);
+//                 } else if (response.query.results != null) {
+//                     console.log("response:\n", response);
+//                     callback(response.query.results.a);
+//                     return;
+//                 } else {
+//                     console.log("no response :( ");
+//                 }
+//             }, {"url": url, "xpath": xpath});
+//             i++;
+//         }
+//     }
+// }
+
 module.exports = {
     makeQuery : function(url, xpath, callback) {
+        console.log("making query...");
         new YQL.exec(query, function(response) {
             if (response.error) {
                 console.log("error with makeQuery: ", response.error.description);
             } else if (response.query.results != null) {
-                //response.qurey.results.a is an ARRAY of js object of the form
-                // {"href": "/game/{GAMEID}", "content": "{TITLE}"}
                 console.log("response:\n", response);
                 callback(response.query.results.a);
             } else {
