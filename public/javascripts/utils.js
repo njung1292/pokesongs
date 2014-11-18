d0bb174b8667f455df317d601f576f87b4dffeb7
Noelle Jung <njung1292@gmail.com>
2014-11-18 17:48:13 -0500

    fixed tooltip and now playing

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -4,10 +4,11 @@ var gameTitles = [];
 var currentIdx = -1;
 var masterGame = "";
 var masterSongList = [];
+var baseUrl = 'http://www.brawlcustommusic.com/music/mp3/';
 // var utils_socket = io.connect('http://localhost:5000/utils');
 var utils_socket = io.connect('http://pokesongs.herokuapp.com/utils');
 
-// Setup socket to respond to songlist data from the server
+// Set up socket to respond to songlist data from the server
 utils_socket.on('data', function (data) {
     masterSongList = data;
     currentIdx = -1;
@@ -21,6 +22,7 @@ function enableButtons() {
     $('#game').css('background-color', '');
     $('#randomGame').removeClass('disabled');
     $('#submit').removeClass('disabled');
+    $('#nowPlaying').text('');
 }
 
 function hashGames(data) {
@@ -53,13 +55,24 @@ function forward() {
     updateNowPlaying(masterSongList[currentIdx], currentIdx);
 }
 
+function pause() {
+    var audio = $('audio');
+    if (audio[0].paused) {
+        audio[0].play();
+        $('#pause-btn').attr('class', 'glyphicon glyphicon-pause');
+    } else {
+        audio[0].pause();
+        $('#pause-btn').attr('class', 'glyphicon glyphicon-play');
+    }
+}
+
 // called whenever a new song is played
 function updateNowPlaying(song, idx) {
     currentIdx = idx;
     songId = song.href.substring(1);
     songTitle = song.content;
     var src = 'http://www.brawlcustommusic.com/music/mp3/' + songId + '.mp3';
-    $('#nowPlaying').text('Now Playing: ' + songTitle);
+    $('#nowPlaying').text(songTitle);
     $('audio').attr('src', src);
 
     // for highlighting the now playing song
@@ -68,47 +81,46 @@ function updateNowPlaying(song, idx) {
     if ($('audio')[0].paused) pause();
 }
 
+function makeListItem(songId, songTitle, href) {
+    var link = $('<a></a>')
+        .addClass('list-group-item clearfix')
+        .attr('id', songId)
+        .attr('href', '#')
+        .appendTo('#songList');
+    var songTitleSpan = $('<span></span>')
+        .text(songTitle)
+        .appendTo(link);
+    var buttonSpan = $('<span></span>')
+        .addClass('pull-right')
+        .appendTo(link);
+    var copyButton = $('<button>Copy link</button>')
+        .addClass('btn btn-xs btn-info copy-button')
+        .attr('data-clipboard-text', href)
+        .click(function(event) {
+            event.stopPropagation();
+        })
+        .tooltip({
+            'animation': 'true',
+            'toggle': 'click', 
+            'title': 'Copied!'
+        })
+        .appendTo(buttonSpan);
+    $('#' + songId).click(function(e) {
+        e.preventDefault();
+        updateNowPlaying({'content': songTitle, 'href': "/" + songId}, $(this).index());
+    });
+    $('#songList').append('<button id="copy-button" data-clipboard-text="' + href + '"');
+}
+
 // called only when a new game is selected
 function listSongs() {
     $('#songList').empty();
     $('#title').text(masterGame);
-    var baseUrl = 'http://www.brawlcustommusic.com/music/mp3/';
     // Add each song to the list as a link
-    // for testing only!
-    // var masterSongList = [{'href':'/1','content':'a'},{'href':'/2','content':'b'},{'href':'/3','content':'c'},{'href':'/4','content':'  d'}]
     for (var i = 0; i < masterSongList.length; i++) {
         var song = masterSongList[i];
-        var songId = song.href.substring(1);
-        var songTitle = song.content;
-        var href = baseUrl + songId + '.mp3';
-        var link = $('<a></a>')
-            .addClass('list-group-item clearfix')
-            .attr('id', songId)
-            .attr('href', '#')
-            .appendTo('#songList');
-        var songTitleSpan = $('<span></span>')
-            .text(songTitle)
-            .appendTo(link);
-        var buttonSpan = $('<span></span>')
-            .addClass('pull-right')
-            .appendTo(link);
-        var copyButton = $('<button>Copy link</button>')
-            .addClass('btn btn-xs btn-info copy-button')
-            .attr('data-clipboard-text', href)
-            .click(function(event) {
-                event.stopPropagation();
-            })
-            .tooltip({
-                'animation': 'true',
-                'trigger': 'click',
-                'title': 'Copied!'
-            })
-            .appendTo(buttonSpan);
-        $('#' + songId).click(function(e) {
-            e.preventDefault();
-            updateNowPlaying({'content': this.text, 'href': "/" + this.id}, $(this).index());
-        });
-        $('#songList').append('<button id="copy-button" data-clipboard-text="' + href + '"');
+        songId = song.href.substring(1);
+        makeListItem(songId, song.content, baseUrl + songId + '.mp3');
         if (i == masterSongList.length - 1) {
             var client = new ZeroClipboard( $('.copy-button') );
         }
@@ -127,15 +139,4 @@ function pickRandom() {
     masterGame = randomGameTitle;
     masterSongList = [];
     utils_socket.emit('game', gameDict[masterGame]);
-}
-
-function pause() {
-    var audio = $('audio');
-    if (audio[0].paused) {
-        audio[0].play();
-        $('#pause-btn').attr('class', 'glyphicon glyphicon-pause');
-    } else {
-        audio[0].pause();
-        $('#pause-btn').attr('class', 'glyphicon glyphicon-play');
-    }
 }
